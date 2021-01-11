2b38389604d488bfbede3b6a1a46b9a350fb1f45
Guillermo <guillermo@ochoadeaspuru.com>
2021-01-11 19:16:29 +0100

    first commit

---
+++ JSwingGui/src/main/java/com/testuality/jswinggui/JSwingGui.java
@@ -0,0 +1,12 @@
+package com.testuality.jswinggui;
+
+/**
+ *
+ * @author testuality
+ */
+public class JSwingGui {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
